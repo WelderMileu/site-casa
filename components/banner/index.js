31c00a6cc8f30a19735fd67c6050adb658b444e2
Welder Mileu <welderfelix21@gmail.com>
2020-11-23 22:00:00 -0300

    adicionando o motion

--- components/banner/index.js
+++ components/banner/index.js
@@ -1,38 +1,120 @@
 import React from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 import { Container, Content, Imagens, Imagem } from './style';
+
+const animacao = {
+	inicia: {
+		opacity: 0,
+		y: 0,
+		x: -30,
+	},
+
+	termina : {
+		opacity: 1,
+		y: 0,
+		x: 0,
+	},
+}
+
+const imagemAnima = {
+	inicia: {
+		opacity: 0,
+		top: -10,
+	},
+	
+	termina: {
+		opacity: 1,
+		top: 0,
+	},
+}
 
 const Banner = () => {
 	return (
 		<Container>
 			<Content>
-				<p>02.</p>
-				<p>Dark</p>
-				<p>Interior</p>
-				<p><div></div>Learn More</p>
+				<motion.p
+					variants={animacao}
+					initial="inicia"
+					animate="termina"
+					transition={{ duration: 1 }}
+				>
+					02.
+				</motion.p>
+
+				<motion.p
+					variants={animacao}
+					initial="inicia"
+					animate="termina"
+					transition={{ duration: 1, delay: 1 }}	
+				>
+					Dark
+				</motion.p>
+
+
+				<motion.p
+					variants={animacao}
+					initial="inicia"
+					animate="termina"
+					transition={{ duration: 1, delay: 1 }}
+				>
+					Interior
+				</motion.p>
+
+				<motion.p
+					variants={animacao}
+					initial="inicia"
+					animate="termina"
+					transition={{ duration: 1, delay: 1.5 }}
+				><div></div>
+					Learn More
+				</motion.p>
+
 			</Content>
 			<Imagens>
-				<Image
-					src="/image/image01.jpg"
-					alt="imagem"
-					width={270}
-					height={500}
-				/>
+				<motion.div
+					variants={imagemAnima}
+					initial="inicia"
+					animate="termina"
+					transition={{ duration: 1, delay: 2 }}
+				>
+					<Image
+						src="/image/image01.jpg"
+						alt="imagem"
+						width={270}
+						height={500}
+					/>
+				</motion.div>
 
-				<Image
-					src="/image/image02.jpg"
-					alt="imagem"
-					width={270}
-					height={500}
-				/>
+				<motion.div
+					variants={imagemAnima}
+					initial="inicia"
+					animate="termina"
+					transition={{ duration: 1, delay: 2.2 }}
+				>
+					<Image
+						src="/image/image02.jpg"
+						alt="imagem"
+						width={270}
+						height={500}
+					/>
+				</motion.div>
 
-				<Image 
-					src="/image/image03.jpg"
-					alt="imagem"
-					width={200}
-					height={500}
-				/>
+				<motion.div
+					variants={imagemAnima}
+					initial="inicia"
+					animate="termina"
+					transition={{ duration: 1, delay: 2.4 }}
+				>
+					<Image 
+						src="/image/image03.jpg"
+						alt="imagem"
+						width={200}
+						height={500}
+					/>
+				</motion.div>
+				
 			</Imagens>
 		</Container>
 	)
--- components/footer/index.js
+++ components/footer/index.js
@@ -1,21 +1,62 @@
 import React from 'react';
 import { Container } from './style';
+import { motion } from 'framer-motion';
+
+const itemLeft = {
+	inicia: {
+		opacity: 0,
+		x: -10,
+		y: 0
+	},
+
+	termina: {
+		opacity: 1,
+		x: 0,
+		y: 0
+	}
+}
+
+const itemRight = {
+	inicia: {
+		opacity: 0,
+		x: 10,
+		y: 0
+	},
+
+	termina: {
+		opacity: 1,
+		x: 0,
+		y: 0
+	}
+}
 
 const Footer = () => {
 	return (
 		<Container>
-			<div>
+			<motion.div
+				variants={itemLeft}
+				initial="inicia"
+				animate="termina"
+				transition={{ transition: .5, delay: 3 }}
+			>
 				<div></div>
 				<div></div>
 				<div></div>
 				<div></div>
 				<div></div>
-			</div>
-			<div>
+			</motion.div>
+
+			<motion.div
+				variants={itemRight}
+				initial="inicia"
+				animate="termina"
+				transition={{ transition: .5, delay: 3 }}	
+			>
 				<p>03 / 06</p>
 				<div></div>
 				<p>></p>
-			</div>
+			</motion.div>
+			
 		</Container>
 	)
 }
